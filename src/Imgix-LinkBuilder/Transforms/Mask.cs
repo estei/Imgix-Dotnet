160b59875b1c7824c102e92be335da0e3273fb0d
Rasmus Lauridsen <rasmus@lauridsen.me>
2015-11-24 20:38:08 +0100

    added mask related extension methods

--- src/Imgix-LinkBuilder/Transforms/Mask.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Imgix_LinkBuilder.Transforms.Mask
-{
-    /// <summary>
-    /// ImgixImage extension methods that set mask related parameters on the Url.
-    /// </summary>
-    public static class Mask
-    {
-    }
-}
