898d92702f9e7b47c97aa2063f359c1ce3052138
Rasmus Lauridsen <rasmus@lauridsen.me>
2015-11-24 17:32:41 +0100

    created the mask classes

---
+++ src/Imgix-LinkBuilder.Tests/Transforms/MaskTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Imgix_LinkBuilder.Tests.Transforms
+{
+    [TestFixture]
+    public class MaskTests
+    {
+
+    }
+}
---
+++ src/Imgix-LinkBuilder/Transforms/Mask.cs
@@ -0,0 +1,9 @@
+﻿namespace Imgix_LinkBuilder.Transforms.Mask
+{
+    /// <summary>
+    /// ImgixImage extension methods that set mask related parameters on the Url.
+    /// </summary>
+    public static class Mask
+    {
+    }
+}
