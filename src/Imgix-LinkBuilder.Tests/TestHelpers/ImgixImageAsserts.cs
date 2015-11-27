e6923c18f09fe72e1f03bae04e87dac1c3eb643e
Rasmus Lauridsen <rasmus@lauridsen.me>
2015-11-27 12:15:28 +0100

    Border extension

--- src/Imgix-LinkBuilder.Tests/TestHelpers/ImgixImageAsserts.cs
+++ src/Imgix-LinkBuilder.Tests/TestHelpers/ImgixImageAsserts.cs
@@ -8,7 +8,7 @@ namespace Imgix_LinkBuilder.Tests.TestHelpers
     {
         public static void HasQueryParameter(ImgixImage subject, string name, string value)
         {
-            Assert.True(new Url(subject).QueryParams.Contains(new KeyValuePair<string, object>(name, value)), $"Expected url to have query parameter {name}={value} but it was not found");
+            Assert.True(new Url(subject).QueryParams.Contains(new KeyValuePair<string, object>(name, value)), $"Expected image url[{subject}] to have query parameter {name}={value} but it was not found.");
         }
     }
 }
---
+++ src/Imgix-LinkBuilder.Tests/Transforms/TransformsTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Imgix_LinkBuilder.Tests.Transforms
+{
+    public class TransformsTests
+    {
+        internal ImgixImage Image;
+
+        [SetUp]
+        public void MainFixtureInit()
+        {
+            Image = Imgix.NewImage(new ImgixOptions("sourceName"), "some/path/to/some/image.jpg");
+        }
+    }
+}
