93981b32198393dbfe16a036af7f6be43599f5a5
Rasmus Lauridsen <rasmus@lauridsen.me>
2015-11-27 13:07:22 +0100

    trim, trimcolor and trimmd

---
+++ src/Imgix-LinkBuilder.Tests/Transforms/TrimExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Imgix_LinkBuilder.Tests.TestHelpers;
+using Imgix_LinkBuilder.Transforms;
+using NUnit.Framework;
+
+namespace Imgix_LinkBuilder.Tests.Transforms
+{
+    [TestFixture]
+    public class TrimExtensionsTests : TransformsTests
+    {
+        public class Trim : TrimExtensionsTests
+        {
+            public void Given_a_string_it_will_add_a_parameter_trim_with_the_value_of_the_string()
+            {
+                ImgixImageAsserts.HasQueryParameter(Image.Trim("auto"), "trim", "auto");
+            }
+        }
+
+        public class TrimColor : TrimExtensionsTests
+        {
+            public void Given_a_string_it_will_add_a_parameter_trimcolor_with_the_value_of_the_string()
+            {
+                ImgixImageAsserts.HasQueryParameter(Image.TrimColor("ff00ff"), "trimcolor", "ff00ff");
+            }
+        }
+
+        public class TrimMeanDifference : TrimExtensionsTests
+        {
+            public void Given_an_integer_it_will_add_a_parameter_trimmd_with_the_value_of_the_integer()
+            {
+                ImgixImageAsserts.HasQueryParameter(Image.TrimMeanDifference(5), "trimmd", "5");
+            }
+        }
+    }
+}
---
+++ src/Imgix-LinkBuilder/Transforms/TrimExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace Imgix_LinkBuilder.Transforms
+{
+    /// <summary>
+    /// ImgixImage extension methods that sets trim related parameters on the Url.
+    /// </summary>
+    public static class TrimExtensions
+    {
+        /// <summary>
+        /// Adds the trim parameter to the image url.
+        /// </summary>
+        /// <param name="image">The image to transform</param>
+        /// <param name="value">
+        /// The trim operation to perform
+        /// Values: auto or color
+        /// </param>
+        /// <returns></returns>
+        public static ImgixImage Trim(this ImgixImage image, string value)
+            => image.AddParameter("trim", value);
+
+        /// <summary>
+        /// Adds the timecolor parameter to the image url
+        /// </summary>
+        /// <param name="image">The image to transform</param>
+        /// <param name="value">
+        ///     A three or six char hex color string
+        /// </param>
+        /// <returns></returns>
+        public static ImgixImage TrimColor(this ImgixImage image, string value)
+            => image.AddParameter("trimcolor", value);
+
+        /// <summary>
+        /// The difference threshhold used when trimming a border.
+        /// Only valid when trim is set to auto
+        /// </summary>
+        /// <param name="image">The image to transform</param>
+        /// <param name="value">
+        ///     A lower value makes the trimming more aggressive
+        ///     Default: 11
+        /// </param>
+        /// <returns></returns>
+        public static ImgixImage TrimMeanDifference(this ImgixImage image, int value)
+            => image.AddParameter("trimmd", value.ToString());
+    }
+}
