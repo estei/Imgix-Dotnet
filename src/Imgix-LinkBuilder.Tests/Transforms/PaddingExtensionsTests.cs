10907b85fc894303b574e112362694d902986fc7
Rasmus Lauridsen <rasmus@lauridsen.me>
2015-11-27 12:29:41 +0100

    padding

---
+++ src/Imgix-LinkBuilder.Tests/Transforms/PaddingExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using Imgix_LinkBuilder.Tests.TestHelpers;
+using Imgix_LinkBuilder.Transforms;
+using NUnit.Framework;
+
+namespace Imgix_LinkBuilder.Tests.Transforms
+{
+    [TestFixture]
+    public class PaddingExtensionsTests : TransformsTests
+    {
+        public class Padding : PaddingExtensionsTests
+        {
+            [Test]
+            public void Given_an_integer_value_it_will_add_a_parameter_pad_with_the_integer_as_value()
+            {
+                ImgixImageAsserts.HasQueryParameter(Image.Padding(20), "pad", "20");
+            }
+        }
+    }
+}
