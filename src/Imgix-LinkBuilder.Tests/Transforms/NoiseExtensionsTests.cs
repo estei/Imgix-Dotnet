5becded6b1196e07ce9366352ad43e6b97ad9ec0
Rasmus Lauridsen <rasmus@lauridsen.me>
2015-11-28 16:07:53 +0100

    Noise extensions

---
+++ src/Imgix-LinkBuilder.Tests/Transforms/NoiseExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Imgix_LinkBuilder.Tests.TestHelpers;
+using Imgix_LinkBuilder.Transforms;
+using NUnit.Framework;
+
+namespace Imgix_LinkBuilder.Tests.Transforms
+{
+    [TestFixture]
+    public class NoiseExtensionsTests : TransformsTests
+    {
+        public class NoiseReductionBlur : NoiseExtensionsTests
+        {
+            public void Given_an_integer_it_will_add_the_nr_parameter_with_the_integer_as_value()
+            {
+                ImgixImageAsserts.HasQueryParameter(Image.NoiseReductionBlur(-100), "nr", "-100");
+            }
+        }
+
+        public class NoiseReductionSharpen : NoiseExtensionsTests
+        {
+            public void Given_an_integer_it_will_add_the_nrs_parameter_with_the_integer_as_value()
+            {
+                ImgixImageAsserts.HasQueryParameter(Image.NoiseReductionSharpen(100), "nr", "100");
+            }
+        }
+    }
+}
---
+++ src/Imgix-LinkBuilder/Transforms/NoiseExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Imgix_LinkBuilder.Transforms
+{
+    /// <summary>
+    /// ImgixImage extension methods that sets noise related parameters on the Url.
+    /// </summary>
+    public static class NoiseExtensions
+    {
+        /// <summary>
+        /// Sets the threshold for noise removal
+        /// </summary>
+        /// <param name="image">The image to transform</param>
+        /// <param name="value">
+        ///     Values -100 - 100
+        ///     Default: 20
+        /// </param>
+        /// <returns></returns>
+        public static ImgixImage NoiseReductionBlur(this ImgixImage image, int value)
+            => image.AddParameter("nr", value.ToString());
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="image">The image to transform</param>
+        /// <param name="value">
+        ///     Values -100 - 100
+        ///     Default: 20
+        /// </param>
+        /// <returns></returns>
+        public static ImgixImage NoiseReductionSharpen(this ImgixImage image, int value)
+            => image.AddParameter("nrs", value.ToString());
+    }
+}
