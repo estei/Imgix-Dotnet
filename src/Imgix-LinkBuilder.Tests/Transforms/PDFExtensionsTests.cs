4cb765c12517976b25d50e592b2633ccf34eaeae
Rasmus Lauridsen <rasmus@lauridsen.me>
2015-11-28 16:21:10 +0100

    PDF page

---
+++ src/Imgix-LinkBuilder.Tests/Transforms/PDFExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using Imgix_LinkBuilder.Tests.TestHelpers;
+using Imgix_LinkBuilder.Transforms;
+using NUnit.Framework;
+
+namespace Imgix_LinkBuilder.Tests.Transforms
+{
+    [TestFixture]
+    public class PDFExtensionsTests : TransformsTests
+    {
+        [Test]
+        public void Given_an_integer_it_should_add_a_parameter_page_with_the_value_of_the_integer()
+        {
+            ImgixImageAsserts.HasQueryParameter(Image.Page(1), "page", "1");
+        }
+    }
+}
