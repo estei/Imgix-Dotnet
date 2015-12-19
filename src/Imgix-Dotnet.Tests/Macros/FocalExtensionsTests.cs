caf1df4539c0532b8e36a3cfd3a5aa64aef7d969
Rasmus Lauridsen <rasmus@lauridsen.me>
2015-12-19 01:05:29 +0100

    Testing Focal macros
    Added focal resize that acts like Umbraco focal resizing

--- src/Imgix-Dotnet.Tests/Macros/FocalExtensionsTests.cs
+++ src/Imgix-Dotnet.Tests/Macros/FocalExtensionsTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Mime;
-using Imgix_Dotnet.Configuration;
+﻿using Imgix_Dotnet.Configuration;
 using Imgix_Dotnet.Macros;
 using Imgix_Dotnet.Tests.TestHelpers;
 using NUnit.Framework;
@@ -19,10 +18,228 @@ namespace Imgix_Dotnet.Tests.Macros
 
         public class FocalCrop : FocalExtensionsTests
         {
-            [Test]
-            public void Given_a_crop_that_fits_inside_the_image_position_and_size_should_maintained()
+            public class GivenCropThatFitsInside : FocalCrop
             {
-                ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(10, 10, 6, 6, 40, 40), "rect", "7,7,6,6");
+                [Test]
+                public void Then_position_and_size_should_maintained()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(10, 10, 6, 6, 40, 40), "rect", "7,7,6,6");
+                }
+
+                [Test]
+                public void But_sticks_out_to_the_left_then_position_should_be_moved_to_left_side_of_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(1, 10, 6, 6, 40, 40), "rect", "0,7,6,6");
+                }
+
+                [Test]
+                public void But_sticks_out_to_the_right_then_position_should_be_moved_to_fit_on_right_side()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(38, 10, 6, 6, 40, 40), "rect", "34,7,6,6");
+                }
+
+                [Test]
+                public void But_sticks_out_on_top_then_position_should_be_moved_to_fit_on_top()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(10, 1, 6, 6, 40, 40), "rect", "7,0,6,6");
+                }
+
+                [Test]
+                public void But_sticks_out_on_the_bottom_then_position_should_be_moved_to_fit_on_the_bottom()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(10, 38, 6, 6, 40, 40), "rect", "7,34,6,6");
+                }
+            }
+
+            public class GivenCropThatIsLarger : FocalCrop
+            {
+                [Test]
+                public void The_crop_should_shrink_to_fit_inside_the_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(10, 10, 50, 50, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void And_a_different_aspect_from_the_image_the_crop_should_shrink_to_fit_inside_the_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(10, 10, 50, 40, 40, 40), "rect", "0,0,40,32");
+                }
+            }
+
+            public class GivenCropThatFitsInsideAndPercentagePositioned : FocalCrop
+            {
+                [Test]
+                public void Then_position_and_size_should_maintained()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(0.5, 0.5, 6, 6, 40, 40), "rect", "17,17,6,6");
+                }
+
+                [Test]
+                public void But_sticks_out_to_the_left_then_position_should_be_moved_to_left_side_of_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(0.1, 0.5, 10, 10, 40, 40), "rect", "0,15,10,10");
+                }
+
+                [Test]
+                public void But_sticks_out_to_the_right_then_position_should_be_moved_to_fit_on_right_side()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(0.99, 0.5, 6, 6, 40, 40), "rect", "34,17,6,6");
+                }
+
+                [Test]
+                public void But_sticks_out_on_top_then_position_should_be_moved_to_fit_on_top()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(0.5, 0.1, 10, 10, 40, 40), "rect", "15,0,10,10");
+                }
+
+                [Test]
+                public void But_sticks_out_on_the_bottom_then_position_should_be_moved_to_fit_on_the_bottom()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(0.5, 0.99, 6, 6, 40, 40), "rect", "17,34,6,6");
+                }
+            }
+
+            public class GivenCropThatIsLargerAndPercentagePositioned : FocalCrop
+            {
+                [Test]
+                public void The_crop_should_shrink_to_fit_inside_the_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(0.5, 0.5, 50, 50, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void And_a_different_aspect_from_the_image_the_crop_should_shrink_to_fit_inside_the_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalCrop(0.5, 0.5, 50, 40, 40, 40), "rect", "0,4,40,32");
+                }
+            }
+
+        }
+
+        public class FocalResize : FocalExtensionsTests
+        {
+            public class GivenSizeThatFitsInside : FocalResize
+            {
+                [Test]
+                public void Then_grow_the_crop_to_match_the_image_before_resizing()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(10, 10, 6, 6, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void
+                    But_sticks_out_to_the_left_then_position_should_be_moved_to_left_side_of_image_and_grow_to_match()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(1, 10, 6, 6, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void
+                    But_sticks_out_to_the_right_then_position_should_be_moved_to_fit_on_right_of_image_and_grow_to_match
+                    ()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(38, 10, 6, 6, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void But_sticks_out_on_top_then_position_should_be_moved_to_fit_on_top_and_grow_to_match()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(10, 1, 6, 6, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void
+                    But_sticks_out_on_the_bottom_then_position_should_be_moved_to_fit_on_the_bottom_and_grow_to_match()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(10, 38, 6, 6, 40, 40), "rect", "0,0,40,40");
+                }
+            }
+
+            public class GivenCropThatIsLarger : FocalResize
+            {
+                [Test]
+                public void The_crop_should_shrink_to_fit_inside_the_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(10, 10, 50, 50, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void And_a_different_aspect_from_the_image_the_crop_should_shrink_to_fit_inside_the_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(10, 10, 50, 40, 40, 40), "rect", "0,0,40,32");
+                }
+            }
+
+            public class GivenCropThatFitsInsideAndPercentagePositioned : FocalResize
+            {
+                [Test]
+                public void Then_grow_the_crop_to_match_the_image_before_resizing()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.5, 0.5, 6, 6, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void
+                    But_sticks_out_to_the_left_then_position_should_be_moved_to_left_side_of_image_and_grow_to_match()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.1, 0.5, 10, 10, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void
+                    But_sticks_out_to_the_right_then_position_should_be_moved_to_fit_on_right_of_image_and_grow_to_match
+                    ()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.99, 0.5, 6, 6, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void But_sticks_out_on_top_then_position_should_be_moved_to_fit_on_top_and_grow_to_match()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.5, 0.1, 10, 10, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void
+                    But_sticks_out_on_the_bottom_then_position_should_be_moved_to_fit_on_the_bottom_and_grow_to_match()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.5, 0.99, 6, 6, 40, 40), "rect", "0,0,40,40");
+                }
+            }
+
+            public class GivenCropThatIsLargerAndPercentagePositioned : FocalResize
+            {
+                [Test]
+                public void The_crop_should_shrink_to_fit_inside_the_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.5, 0.5, 50, 50, 40, 40), "rect", "0,0,40,40");
+                }
+
+                [Test]
+                public void And_a_different_aspect_from_the_image_the_crop_should_shrink_to_fit_inside_the_image()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.5, 0.5, 50, 40, 40, 40), "rect", "0,4,40,32");
+                }
+            }
+
+            public class GivenAWidth : FocalResize
+            {
+                [Test]
+                public void A_w_property_should_be_added_to_the_url()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.5, 0.5, 50, 50, 40, 40), "w", "50");
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(10, 10, 50, 50, 40, 40), "w", "50");
+                }
+            }
+
+            public class GivenAHeight : FocalResize
+            {
+                [Test]
+                public void An_h_property_should_be_added_to_the_url()
+                {
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(0.5, 0.5, 50, 50, 40, 40), "h", "50");
+                    ImgixImageAsserts.HasQueryParameter(Image.FocalResize(10, 10, 50, 50, 40, 40), "h", "50");
+                }
             }
         }
     }
